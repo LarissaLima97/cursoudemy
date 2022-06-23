5c4473bf30aa627301933a288082314a9021225e
LarissaLima97 <larissa.lmaria@gmail.com>
2022-06-23 19:30:47 -0300

    Aprendendo alert, prompt

---
+++ javascript-basico/aula21/script.js
@@ -0,0 +1,5 @@
+let num1 = Number(prompt("Digite o primeiro número: "))
+let num2 = Number(prompt("Digite o segundo número: "))
+let resultado = num1 + num2 
+
+alert(`O resultado é : ${resultado}  `)
---
+++ javascript-basico/aula23/index.js
@@ -0,0 +1,16 @@
+let a = 'A' // B
+let b = 'B' // C
+let c = 'C' // A 
+
+console.log(a + b)
+
+const auxiliar = a 
+a = b 
+b = c 
+c = auxiliar 
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+
