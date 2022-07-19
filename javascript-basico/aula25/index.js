f833f1a9670bbf5521000a5000f39fae67d9a7df
LarissaLima97 <larissa.lmaria@gmail.com>
2022-07-19 19:38:10 -0300

    Refatorando as variáveis

--- javascript-basico/aula25/index.js
+++ javascript-basico/aula25/index.js
@@ -1,13 +1,5 @@
 
 const nome = prompt('Digite seu nome completo:');
-const quantidadeLetras = nome.length;
-const segundaLetra = nome.charAt(1);
-const indexPrimeiraLetra = nome.indexOf("L");
-const indexUltimaLetra = nome.lastIndexOf("a");
-const ultimasTresLetras = nome.slice(-3); //tamanho da string -3 
-const palavrasNome = nome.split(" ");
-const nomeMaisuclo = nome.toUpperCase();
-const nomeMinusculo= nome.toLowerCase();
 
 document.body.innerHTML += `Seu nome é: <strong>${nome}</strong><br />`;
 document.body.innerHTML += `Seu nome tem <strong>${nome.length}</strong> letras <br />`;
@@ -18,6 +10,3 @@ document.body.innerHTML += `As últimas 3 letras do seu nome são: <strong>${nom
 document.body.innerHTML += `As palavras do seu nome são:  <strong>${nome.split(" ")}</strong><br />`;
 document.body.innerHTML += `Seu nome com letras maiúsculas: <strong>${nome.toUpperCase()}</strong><br />`;
 document.body.innerHTML += `Seu nome com letras minúsculas: <strong>${nome.toLowerCase()}</strong><br />`;
-
-
-    
