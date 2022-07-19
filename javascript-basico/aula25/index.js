cc14ac4c3cfeb46e407f2e6588dcd05a172a290f
LarissaLima97 <larissa.lmaria@gmail.com>
2022-07-19 19:25:51 -0300

    Refatorando as variavéis

--- javascript-basico/aula25/index.js
+++ javascript-basico/aula25/index.js
@@ -9,15 +9,15 @@ const palavrasNome = nome.split(" ");
 const nomeMaisuclo = nome.toUpperCase();
 const nomeMinusculo= nome.toLowerCase();
 
-document.body.innerHTML += `Seu nome é: ${nome}<br />`;
-document.body.innerHTML += `Seu nome tem ${quantidadeLetras} letras <br />`;
-document.body.innerHTML += `A segunda letra do seu nome é: ${segundaLetra}<br />`;
-document.body.innerHTML += `Qual o primeiro índice da letra L no seu nome? ${indexPrimeiraLetra}<br />`;
-document.body.innerHTML += `Qual o último índice da letra A no seu nome? ${indexUltimaLetra}<br />`;
-document.body.innerHTML += `As últimas 3 letras do seu nome são: ${ultimasTresLetras}<br />`;
-document.body.innerHTML += `As palavras do seu nome são: ${palavrasNome}<br />`;
-document.body.innerHTML += `Seu nome com letras maiúsculas: ${nomeMaisuclo}<br />`;
-document.body.innerHTML += `Seu nome com letras minúsculas: ${nomeMinusculo}<br />`;
+document.body.innerHTML += `Seu nome é: <strong>${nome}</strong><br />`;
+document.body.innerHTML += `Seu nome tem <strong>${nome.length}</strong> letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é:  <strong>${nome.charAt(1)}</strong><br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra L no seu nome? <strong>${nome.indexOf("L")}</strong><br />`;
+document.body.innerHTML += `Qual o último índice da letra A no seu nome?  <strong>${nome.lastIndexOf("a")}</strong><br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: <strong>${nome.slice(-3)}</strong><br />`;
+document.body.innerHTML += `As palavras do seu nome são:  <strong>${nome.split(" ")}</strong><br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: <strong>${nome.toUpperCase()}</strong><br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: <strong>${nome.toLowerCase()}</strong><br />`;
 
 
     
