f9206b4eb9db0804a78d93088eb1c4db63982057
LarissaLima97 <larissa.lmaria@gmail.com>
2023-05-11 19:21:39 -0300

    Implementando a lógica no botão calcular

--- javascript-basico/aula44-IMC/assets/js/main.js
+++ javascript-basico/aula44-IMC/assets/js/main.js
@@ -1,28 +1,37 @@
-function calculaIMC (peso,altura) {
-    if(typeof peso && typeof altura === 'number' ){
-        const imc = peso / (altura * altura);
+function calculaIMC (e) {
+    e.preventDefault();
+
+    const peso = Number(document.querySelector("#peso").value);
+    console.log("Valor do peso", typeof peso)
+    const altura = Number(document.querySelector("#altura").value);
+    console.log("Valor da Altura", typeof altura)
+    const resultado = document.querySelector("#resultado");
+
+    if(typeof peso === 'number' && typeof altura === 'number'){
+        const imc = Math.round(peso / (altura * altura));
         if(imc < 18.5){
-            return "Abaixo do peso";
+            resultado.innerHTML = `<p style="background-color:rgba(221, 49, 43, 0.575);">Seu IMC é ${imc} (Abaixo do peso)</p>`;
         }
         else if(imc === 18.5 || imc <=24.9){
-            return "Peso normal";
+            resultado.innerHTML = `<p style="background-color:rgba(97, 245, 83, 0.575);">Seu IMC é ${imc} (Peso normal)</p>`;
         }
         else if(imc === 25 || imc <= 29.9){
-            return "Sobrepeso"
+            resultado.innerHTML = `<p style="background-color:rgba(236, 218, 55, 0.575);">Seu IMC é ${imc} (Sobrepeso)</p>`;
         }
         else if(imc === 30 || imc <= 34.29){
-            return "Obesidade grau 1"
+            resultado.innerHTML = `<p style="background-color:rgba(233, 150, 56, 0.575);">Seu IMC é ${imc} (Obesidade grau 1)</p>`;
         }
         else if(imc === 35 || imc <= 39.9){
-            return "Obesidade grau 2"
+            resultado.innerHTML = `<p style="background-color:rgba(236, 61, 55, 0.575);">Seu IMC é ${imc} (Obesidade grau 2)</p>`;
         }
         else if(imc > 40){
-            return "Obesidade grau 3"
+            resultado.innerHTML = `<p style="background-color:rgba(236, 61, 55, 0.575);">Seu IMC é ${imc} (Obesidade grau 3)</p>`;
+        }
+        else {
+            resultado.innerHTML = `<p style="background-color:rgba(236, 61, 55, 0.575)";>Altura ou peso inválidos!</p>`;
         }
     }
-
-    return "Digite números";
-
 }
 
-console.log(calculaIMC( "oi", "vc"));
+const botaoCalcular = document.querySelector("#botao-calc");
+botaoCalcular.addEventListener("click", calculaIMC);
