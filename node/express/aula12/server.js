b47ee1df0b6d414e5df7a81a251d109aa02e3416
LarissaLima97 <larissa.lmaria@gmail.com>
2024-12-02 14:58:39 -0300

    Adicionando json

--- node/express/aula12/server.js
+++ node/express/aula12/server.js
@@ -20,13 +20,8 @@ const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
 app.use(helmet());
-app.use(
-    express.urlencoded(
-        {
-            extended:true
-        } 
-    )
-);
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
